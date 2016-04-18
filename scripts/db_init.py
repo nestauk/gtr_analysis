5f4d0a9b9ba3140bc41d079f1e18995b22486250
James Gardiner <jamesg87@me.com>
2016-04-18 13:31:08 +0100

    Update db_init

--- scripts/db_init.py
+++ scripts/db_init.py
@@ -10,105 +10,113 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.schema import CreateSchema
 
 
-def main():
+def get_configs(in_file='config.json'):
     # Read in user configs
-    with open('config.json') as f:
+    with open(in_file) as f:
         conf = json.load(f)
+    return conf
 
-    # Assign to local variables
-    user = conf['user']
-    host = conf['host']
-    port = conf['port']
-    passw = conf['passw']
-    schema = conf['schema']
-    database = conf['database']
+conf = get_configs()
+user = conf['user']
+host = conf['host']
+port = conf['port']
+passw = conf['passw']
+schema = conf['schema']
+database = conf['database']
 
-    # Base object that remains aware
-    # of subclasses
-    Base = declarative_base()
+# Base object that remains aware
+# of subclasses
+Base = declarative_base()
 
-    # Table specifications
-    class Project(Base):
-        """Define the Project object schema"""
-        __tablename__ = 'projects'
-        __table_args__ = {"schema": schema}
-        pkey = Column(Integer, primary_key=True, autoincrement=True)
-        abstract_text = Column(Text)
-        created = Column(DateTime)
-        grant_cat = Column(Text)
-        href = Column(Text)
-        health_categories = Column(JSONB)
-        id = Column(Text)
-        identifiers = Column(JSONB)
-        lead_org_dpt = Column(Text)
-        links = (Column(JSONB))
-        potential_impact = Column(Text)
-        researchActivities = Column(JSONB)
-        research_subjects = Column(JSONB)
-        research_topics = Column(JSONB)
-        status = Column(Text)
-        title = Column(Text)
 
-    # Table specifications
-    class Person(Base):
-        """Define the Project object schema"""
-        __tablename__ = 'persons'
-        __table_args__ = {"schema": schema}
-        pkey = Column(Integer, primary_key=True, autoincrement=True)
-        created = Column(DateTime)
-        first_name = Column(Text)
-        href = Column(Text)
-        id = Column(Text)
-        links = Column(JSONB)
-        surname = Column(Text)
+# Table specifications
+class Project(Base):
+    """Define the Project object schema"""
+    __tablename__ = 'projects'
+    __table_args__ = {"schema": schema}
+    pkey = Column(Integer, primary_key=True, autoincrement=True)
+    abstract_text = Column(Text)
+    created = Column(DateTime)
+    grant_cat = Column(Text)
+    href = Column(Text)
+    health_categories = Column(JSONB)
+    id = Column(Text, unique=True)
+    identifiers = Column(JSONB)
+    lead_org_dpt = Column(Text)
+    links = (Column(JSONB))
+    potential_impact = Column(Text)
+    researchActivities = Column(JSONB)
+    research_subjects = Column(JSONB)
+    research_topics = Column(JSONB)
+    status = Column(Text)
+    title = Column(Text)
 
-    # Table specifications
-    class Publication(Base):
-        """Define the Publication object schema"""
-        __tablename__ = 'publications'
-        __table_args__ = {"schema": schema}
-        pkey = Column(Integer, primary_key=True, autoincrement=True)
-        abstract_text = Column(Text)
-        created = Column(DateTime)
-        href = Column(Text)
-        id = Column(Text, primary_key=True)
-        links = Column(JSONB)
-        title = Column(Text)
-        pub_type = Column(Text)
-        journal_title = Column(Text)
-        doc = Column(JSONB)
-        published_date = Column(DateTime)
-        url = Column(Text)
-        vol = Column(Text)
-        doi = Column(Text)
-        issue = Column(Text)
-        author = Column(Text)
 
-    # Table specifications
-    class Organisation(Base):
-        """Define the Organisation object schema"""
-        __tablename__ = 'organisations'
-        __table_args__ = {"schema": schema}
-        pkey = Column(Integer, primary_key=True, autoincrement=True)
-        addresses = Column(JSONB)
-        created = Column(DateTime)
-        href = Column(Text)
-        id = Column(Text, primary_key=True)
-        links = Column(JSONB)
-        name = Column(Text)
+# Table specifications
+class Person(Base):
+    """Define the Project object schema"""
+    __tablename__ = 'persons'
+    __table_args__ = {"schema": schema}
+    pkey = Column(Integer, primary_key=True, autoincrement=True)
+    created = Column(DateTime)
+    first_name = Column(Text)
+    href = Column(Text)
+    id = Column(Text, unique=True)
+    links = Column(JSONB)
+    surname = Column(Text)
 
-    class Fund(Base):
-        """Define the fund object schema"""
-        __tablename__ = 'funds'
-        __table_args__ = {"schema": schema}
-        pkey = Column(Integer, primary_key=True, autoincrement=True)
-        category = Column(Text)
-        href = Column(Text)
-        links = Column(JSONB)
-        value_pounds = Column(Integer)
-        start = Column(DateTime)
-        end = Column(DateTime)
 
+# Table specifications
+class Publication(Base):
+    """Define the Publication object schema"""
+    __tablename__ = 'publications'
+    __table_args__ = {"schema": schema}
+    pkey = Column(Integer, primary_key=True, autoincrement=True)
+    abstract_text = Column(Text)
+    created = Column(DateTime)
+    href = Column(Text)
+    id = Column(Text, unique=True)
+    links = Column(JSONB)
+    title = Column(Text)
+    pub_type = Column(Text)
+    journal_title = Column(Text)
+    doc = Column(JSONB)
+    published_date = Column(DateTime)
+    url = Column(Text)
+    vol = Column(Text)
+    doi = Column(Text)
+    issue = Column(Text)
+    author = Column(Text)
+
+
+# Table specifications
+class Organisation(Base):
+    """Define the Organisation object schema"""
+    __tablename__ = 'organisations'
+    __table_args__ = {"schema": schema}
+    pkey = Column(Integer, primary_key=True, autoincrement=True)
+    addresses = Column(JSONB)
+    created = Column(DateTime)
+    href = Column(Text)
+    id = Column(Text, unique=True)
+    links = Column(JSONB)
+    name = Column(Text)
+
+
+class Fund(Base):
+    """Define the fund object schema"""
+    __tablename__ = 'funds'
+    __table_args__ = {"schema": schema}
+    pkey = Column(Integer, primary_key=True, autoincrement=True)
+    category = Column(Text)
+    href = Column(Text, unique=True)
+    links = Column(JSONB)
+    value_pounds = Column(Integer)
+    start = Column(DateTime)
+    end = Column(DateTime)
+
+
+def main():
     # Connection string using config options
     conn_str = 'postgresql://{}:{}@{}:{}/{}'.format(user,
                                                     passw,
